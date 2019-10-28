a5f367206ee3c2e53cec0b8e053d2de3589b29da
Maryna Ulasava <marynaulasava@gmail.com>
2019-10-28 20:53:06 +0100

    he quitado la variable b para calcular la base ,porque al final no ha hecho falta

--- src/piramidedeasteriscos/PiramideDeAsteriscos.java
+++ src/piramidedeasteriscos/PiramideDeAsteriscos.java
@@ -7,18 +7,13 @@ package piramidedeasteriscos;
 import java.util.*;
 
 
-/**TRAPECIO 
- *
- * @author M
- */
 public class PiramideDeAsteriscos {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese altura de la piramide");
         int a = sc.nextInt();
-        int b=(a*2)-1;
-        
+     //   int b=(a*2)-1;//base de la piramide
         for(int i=0; i<a;i++){
             for(int j=0 ;j<i+a; j++){
             if((i+j)<=a){
