bc9b7960fee86652b63928346279944a524807ef
Maryna Ulasava <marynaulasava@gmail.com>
2019-10-28 11:44:27 +0100

    TRAPECIO RECTANGULO

---
+++ src/piramidedeasteriscos/PiramideDeAsteriscos.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piramidedeasteriscos;
+import java.util.*;
+
+
+/**TRAPECIO RECTANGULO
+ *
+ * @author M
+ */
+public class PiramideDeAsteriscos {
+
+    /**
+     * @param args the command line arguments  System.out.println("Ingrese altura de la piramide");
+        int a = sc.nextInt();
+        int b=(a*2)-1;
+        
+        for(int i=0; i<a;i++){
+            for(int j=0 ;j<b; j++){
+                           
+    }
+        
+            Syste
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese altura de la piramide");
+        int a = sc.nextInt();
+        int b=(a*2)-1;
+        
+        for(int i=0; i<a;i++){
+            for(int j=0 ;j<i+a; j++){
+    
+            System.out.print(" *");}
+            
+            System.out.println();
+}
+    }
+}
+
